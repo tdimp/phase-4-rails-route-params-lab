2e1ecf9f0e3f612a74c21c5b55d98a937dd7461a
Thomas Dimperio <twdimperio@gmail.com>
2022-07-13 09:29:48 -0600

    commit tree 46b4f0b668b87d2f8dcdc334650368a40c0a66b8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,13 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+
+    student = Student.find_by(last_name: params[:name])
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
